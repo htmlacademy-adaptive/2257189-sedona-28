bbd42eeb50603dc7dd2c9c69c0deed53f8bf49d7
Tkachenko88 <vanes_09@inbox.ru>
2023-05-20 14:54:54 +0300

    оптимизаци и пиксель перфект

--- source/js/script.js
+++ source/js/script.js
@@ -1,9 +1,9 @@
-let header = document.querySelector('.main-nav');
-let navToggle = document.querySelector('.main-nav__toggle');
-let navList = document.querySelector('.main-nav__wrapper')
+let header = document.querySelector(".main-nav");
+let navToggle = document.querySelector(".main-nav__toggle");
+let navList = document.querySelector(".main-nav__wrapper");
 
-header.classList.remove('main-nav--nojs');
+header.classList.remove("main-nav--nojs");
 
-navToggle.addEventListener('click', function () {
-  navList.classList.toggle('main-nav--closed')
+navToggle.addEventListener("click", function () {
+  navList.classList.toggle("main-nav__wrapper--closed");
 });
